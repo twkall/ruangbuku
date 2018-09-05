9d223c5ebd66ae17a39080cecbfa884d3ff14f85
Muhamad Iqbal Tawakal <mcbalbal@live.com>
2018-09-05 10:12:23 +0700

    update lagi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-
+import 'package:validate/validate.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -183,6 +183,23 @@ class _LoginData {
 class _LoginScreenState extends State<LoginScreen> {
   final GlobalKey<FormState> _keyLogin = GlobalKey<FormState>();
 
+  //validation function
+  String _validateEmail(String value){
+    if(!Validate.isEmail(value)){
+      return "Email salah";
+    }
+
+    return null;
+  }
+
+  String _validatePass(String value){
+    if(value.length < 8){
+      return "Password kurang dari 8 karakter";
+    }
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -196,128 +213,153 @@ class _LoginScreenState extends State<LoginScreen> {
           },
         ),
       ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: Color(0xff01baef),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xff01baef),
+              ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                RichText(
-                  textAlign: TextAlign.left,
-                  text: TextSpan(
-                    style: TextStyle(
-                      fontFamily: 'Lato',
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.white,
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                        ),
+                        RichText(
+                          textAlign: TextAlign.left,
+                          text: TextSpan(
+                            style: TextStyle(
+                              fontFamily: 'Lato',
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white,
+                            ),
+                            children: <TextSpan>[
+                              TextSpan(text: "Selamat Datang"),
+                              TextSpan(
+                                text: "\nSilahkan Login untuk Melanjutkan",
+                                style: TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    children: <TextSpan>[
-                      TextSpan(text: "Selamat Datang"),
-                      TextSpan(
-                        text: "\nSilahkan Login untuk Melanjutkan",
-                        style: TextStyle(
-                            fontSize: 12.0, fontWeight: FontWeight.normal),
-                      ),
-                    ],
                   ),
-                ),
-                Form(
-                  key: this._keyLogin,
-                  child: ListView(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 4.0),
-                        child: TextFormField(
-                          style: TextStyle(
-                            color: Color(0xff01baef),
-                            fontFamily: 'Lato',
-                          ),
-                          key: Key("email"),
-                          autofocus: true,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            hintText: "Email",
-                            hintStyle: TextStyle(
-                              color: Color(0xff01baef),
-                              fontFamily: 'Lato',
-                            ),
-                            fillColor: Colors.white,
-                            filled: true,
-                            contentPadding: EdgeInsets.all(12.0),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16.0),
-                              borderSide: BorderSide(
-                                color: Colors.white,
-                                width: 2.0,
+                  Expanded(
+                    flex: 8,
+                    child: Container(
+                      child: Form(
+                        key: this._keyLogin,
+                        child: ListView(
+                          children: <Widget>[
+                            Container(
+                              padding:
+                                  EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 4.0),
+                              child: TextFormField(
+                                style: TextStyle(
+                                  color: Color(0xff01baef),
+                                  fontFamily: 'Lato',
+                                ),
+                                key: Key("email"),
+                                autofocus: true,
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: InputDecoration(
+                                  hintText: "Email",
+                                  hintStyle: TextStyle(
+                                    color: Color(0xff01baef),
+                                    fontFamily: 'Lato',
+                                  ),
+                                  fillColor: Colors.white,
+                                  filled: true,
+                                  contentPadding: EdgeInsets.all(8.0),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(16.0),
+                                    borderSide: BorderSide(
+                                      color: Colors.white,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(16.0),
+                                    borderSide: BorderSide(
+                                      color: Colors.white,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16.0),
-                              borderSide: BorderSide(
-                                color: Colors.white,
-                                width: 2.0,
+                            Container(
+                              padding:
+                                  EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 4.0),
+                              child: TextFormField(
+                                obscureText: true,
+                                style: TextStyle(
+                                  color: Color(0xff01baef),
+                                  fontFamily: 'Lato',
+                                ),
+                                key: Key("password"),
+                                autofocus: true,
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: InputDecoration(
+                                  hintText: "Kata Sandi",
+                                  hintStyle: TextStyle(
+                                    color: Color(0xff01baef),
+                                    fontFamily: 'Lato',
+                                  ),
+                                  fillColor: Colors.white,
+                                  filled: true,
+                                  contentPadding: EdgeInsets.all(8.0),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(16.0),
+                                    borderSide: BorderSide(
+                                      color: Colors.white,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(16.0),
+                                    borderSide: BorderSide(
+                                      color: Colors.white,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
+                            Container(
+                              padding: EdgeInsets.all(16.0),
+                              child: RaisedButton(
+                                child: Text("Masuk"),
+                                textColor: Colors.white,
+                                color: Color(0xff01baef),
+                                onPressed: () => print("Pressed Blue"),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      Container(
-                        padding: EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 4.0),
-                        child: TextFormField(
-                          obscureText: true,
-                          style: TextStyle(
-                            color: Color(0xff01baef),
-                            fontFamily: 'Lato',
-                          ),
-                          key: Key("password"),
-                          autofocus: true,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            hintText: "Kata Sandi",
-                            hintStyle: TextStyle(
-                              color: Color(0xff01baef),
-                              fontFamily: 'Lato',
-                            ),
-                            fillColor: Colors.white,
-                            filled: true,
-                            contentPadding: EdgeInsets.all(12.0),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16.0),
-                              borderSide: BorderSide(
-                                color: Colors.white,
-                                width: 2.0,
-                              ),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16.0),
-                              borderSide: BorderSide(
-                                color: Colors.white,
-                                width: 2.0,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(16.0),
-                        child: RaisedButton(
-                          child: Text("Masuk"),
-                          textColor: Colors.white,
-                          color: Color(0xff01baef),
-                          onPressed: () => print("Pressed Blue"),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
